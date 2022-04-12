0c0d51a3b601c632c64b5d68ede1adddfe4fe6ef
cmeriz <cmerizalde27@gmail.com>
2022-04-12 00:09:03 -0500

    Single File Input

--- modules/inputs.js
+++ modules/inputs.js
@@ -1,7 +1,9 @@
 import customPlaceholder from './inputs/custom-placeholder.js';
 import password from './inputs/password.js';
+import fileSingle from './inputs/file-single.js';
 
 export default function () {
     customPlaceholder();
     password();
+    fileSingle();
 }
--- partials/inputs/_advanced.php
+++ partials/inputs/_advanced.php
@@ -27,6 +27,26 @@
             </button>
         </div>
 
+        <div class="form-input form-input--file-single">
+            <label class="form-input__label" for="file">Single File</label>
+            <div class="form-input form-input--file-single__custom-field-container">
+                <div class="form-input--file-single__custom-field">
+                    <span class="form-input--file-single__label" data-placeholder="Filename">
+                        Filename
+                    </span>
+                    <button class="form-input--file-single__btn-remove">
+                        <svg class="feather">
+                            <use href="./src/icons/feather-sprite.svg#x" />
+                        </svg>
+                    </button>
+                </div>
+                <button class="form-input--file-single__btn-upload">
+                    Select file
+                </button>
+            </div>
+            <input id="file" type="file" class="form-input--file-single__field">
+        </div>
+
     </div>
 
     <div class="card--component__code">
